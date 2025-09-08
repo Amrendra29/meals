cb31abb7c0e525960a43d4c949ea41f6cd83feed
Amrendra Singh <happysingh0987688@gmail.com>
2025-09-08 11:08:07 +0530

    start
    complete the home page with null tapable widgets

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,36 @@
+import 'package:meals/models/category.dart';
+import 'package:flutter/material.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withValues(alpha: 0.55),
+              category.color.withValues(alpha: 0.99),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+        ),
+      ),
+    );
+  }
+}
