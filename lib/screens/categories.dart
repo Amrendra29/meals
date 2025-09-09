8c2d4b5d65367369f795c825e8b073644b7d718f
Amrendra Singh <happysingh0987688@gmail.com>
2025-09-09 20:41:10 +0530

    changing screen
    complete the one screen to other screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: 'Some Title', meals: []),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +29,12 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onselectCategory: () {
+                _selectCategory(context);
+              },
+            ),
         ],
       ),
     );
