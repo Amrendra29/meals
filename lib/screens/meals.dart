ead42202a9b8c916d7835a562eb1dd0ccdb52737
Amrendra Singh <happysingh0987688@gmail.com>
2025-09-10 23:03:55 +0530

    Meal Category
    Update the meal category data . basically second screen updated with text and images

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
   final List<Meal> meals;
-
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +34,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(meals[index].title),
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
       );
     }
     return Scaffold(
